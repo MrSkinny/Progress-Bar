ce07fb5f756239db469dcd155c118dd6cbdffba4
Rich <rgnet1@yahoo.com>
2016-07-18 22:58:26 +0000

    end bracket placement

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Utils = require('./Utils');
 var bar = new ProgressBar();
 bar.start(
   () => {
-    process.stdout.write('Loading... [ ' + Utils.printBlank(this.endIndex / 10 * 2) + ']' );
+    process.stdout.write('Loading... [ ' + Utils.printBlank(bar.endIndex / 10 * 3) + ']' );
     process.stdout.cursorTo(13);
   },
   (progress) => process.stdout.write(progress.toString() + ' '),
